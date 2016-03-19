153cbd2975282aff4999f486fef5f46d33bb2675
mickythump <mikolaj06120@gmail.com>
2016-03-19 17:50:21 +0100

    step 2

---
+++ app/components/Main.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var Main = React.createClass({
+
+  render: function() {
+    return (
+      <div>Hello world!</div>
+    )
+  }
+
+});
+
+module.exports = Main;
